6a27adeb1eb9cc4a3789b5e11709630a525e5964
aayeah <444fck@gmail.com>
2018-12-09 16:28:44 +1000

    need help with assert

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <cstring>
 #include "src/translit.h"
+#include <cassert>
 
 int main() {
     // use only small letters please
-    std::cout << translit((const unsigned char *) "здарова бандиты!") << std::endl;
+    std::cout << translit((const unsigned char*)"привет, как дела?") << std::endl;
 
+    // assert rugaet'sa pomogite
+    //assert(((char*)"privet, kak dela?") == (translit((const unsigned char*)"привет, как дела?")));
     return 0;
 }
