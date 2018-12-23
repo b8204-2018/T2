160539ae57ed44e7b3dd408484b2d98bfcd4bd43
reFreshD2 <44308263+reFreshD2@users.noreply.github.com>
2018-12-23 18:14:12 +1000

    Add files via upload
    Complete tests

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <cstring>
 #include "src/translit.h"
+#include "src/translit.h"
 
 int main() {
     assert(strcmp("privet", translit((const unsigned char *) "привет")) == 0);
+    assert(strcmp("DROU", translit((const unsigned char *) "ДРОУ")) == 0);
+    assert(strcmp("YA pOvAr", translit((const unsigned char *) "Я пОвАр")) == 0);
+    assert(strcmp("SCHavel\'", translit((const unsigned char *) "Щавель")) == 0);
+    assert(strcmp("SHishka", translit((const unsigned char *) "Шишка")) == 0);
     return 0;
 }
