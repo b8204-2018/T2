5848760b6dd1ea1e1908a3afb031e5a1a383a260
reload956 <igor.kokorin.992@gmail.com>
2018-12-24 20:24:30 +1000

    123

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <cstring>
 #include "src/translit.h"
+#include <cassert>
+
 
 int main() {
-    assert(strcmp("privet", translit((const unsigned char *) "привет")) == 0);
+ assert(strcmp("PRIVET", translit((const unsigned char *) "ПРИВЕТ")) == 0);
+    assert(strcmp("KRASOTISHYA", translit((const unsigned char *) "КРАСОТИЩА")) == 0);
+    assert(strcmp("SNOVYMGODOM", translit((const unsigned char *) "С НОВЫМ ГОДОМ")) == 0);
     return 0;
 }
