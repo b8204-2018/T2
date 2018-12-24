543aff238b403e6112b4003a1e0c7b20083407b2
reload956 <igor.kokorin.992@gmail.com>
2018-12-24 20:25:26 +1000

    123

--- src/translit.cpp
+++ src/translit.cpp
@@ -1,6 +1,28 @@
 #include "translit.h"
+#include <string.h>
+#include <iostream>
+#include <cstring>
 
+using namespace std;
 char *translit(const unsigned char *s) {
-    /** TODO: paste your code here */
-    return nullptr;
+    const char sbl[][4]{"A","B","V","G","D","E","ZH","Z","I","IY","K","L","M","N",
+                        "O","P","R","S","T","U","F","HA","TS","CH","SH", "SHY","''",
+                        "Y","`","EH","YU","YA"};
+    int j=0;
+    while (s[j]!=0){
+        j++;
+    };
+    //j/=2;
+    int k =0;
+    int position = 0;
+    char *result = new char[j];
+    for (int i=0;i<j;i++){
+        if (s[i]>=0xd0 && s[i]<=0xd1) {
+            position = (s[i + 1] - 0x90);
+            memcpy(result + k, sbl[position], strlen(sbl[position]));
+            k = k+strlen(sbl[position]);
+        }
+        }
+
+    return result;
 }
