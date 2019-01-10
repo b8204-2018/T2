f0c9a4a241d928b96ff879802702d68be97352d3
SpiderKisa <aleks2000sandra@gmail.com>
2019-01-10 18:08:48 +1000

    ready

--- main.cpp
+++ main.cpp
@@ -4,15 +4,11 @@
 #include <cassert>
 
 int main() {
-    //проблемы с этими 3мя буквами
-    char *s = translit((const unsigned char *) "Ййш"); //Отображаются как "�\231�\271�\210"
-    //pos не вычисляется
-    //для других букв вроде работает
-    assert(strcmp(translit((const unsigned char *) "Moroz i solntse; den' chudesnyj!"), "Мороз и солнце; день чудесный!") == 0);
-    assert(strcmp(translit((const unsigned char *) "Eshche ty dremlesh', drug prelestnyj"), "Еще ты дремлешь, друг прелестный —") == 0);
-    assert(strcmp(translit((const unsigned char *) "Pora, krasavitsa, prosnis':"), "Пора, красавица, проснись:") == 0);
-    assert(strcmp(translit((const unsigned char *) "Otkroy somknuty negoj vzory"), "Открой сомкнуты негой взоры") == 0);
-    assert(strcmp(translit((const unsigned char *) "Navstrechu severnoj Avrory,"), "Навстречу северной Авроры,") == 0);
-    assert(strcmp(translit((const unsigned char *) "Zvezdoyu severa yavis'!"), "Звездою севера явись!") == 0);
+    assert(strcmp("Moroz i solntse; den' chudesnyj!", translit((const unsigned char *)"Мороз и солнце; день чудесный!")) == 0);
+    assert(strcmp("Eshche ty dremlesh', drug prelestnyj —", translit((const unsigned char *)"Еще ты дремлешь, друг прелестный —")) == 0);
+    assert(strcmp("Pora, krasavitsa, prosnis':", translit((const unsigned char *)"Пора, красавица, проснись:")) == 0);
+    assert(strcmp("Otkroj somknuty negoj vzory", translit((const unsigned char *)"Открой сомкнуты негой взоры")) == 0);
+    assert(strcmp("Navstrechu severnoj Avrory,", translit((const unsigned char *)"Навстречу северной Авроры,")) == 0);
+    assert(strcmp("Zvezdoyu severa yavis'!", translit((const unsigned char *)"Звездою севера явись!")) == 0);
     return 0;
 }
--- src/translit.cpp
+++ src/translit.cpp
@@ -3,19 +3,19 @@
 
 char *translit(const unsigned char *s) {
     const unsigned char rus[][3] = {"А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "Й", "К",
-                           "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х",
-                           "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
-                           "а", "б", "в", "г", "д", "е", "ж", "з", "и", "к", "л",
-                           "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц",
-                           "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"};
+                                    "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х",
+                                    "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
+                                    "а", "б", "в", "г", "д", "е", "ж", "з", "и","й", "к", "л",
+                                    "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц",
+                                    "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"};
 
 
     const char eng[][5] = {"A", "B", "V", "G", "D", "E", "Zh", "Z", "I", "J", "K",
                            "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "H",
                            "Ts", "Ch", "Sh", "Shch", "", "Y", "'", "E", "Yu", "Ya",
-                           "a", "b", "v", "g", "d", "e", "zh", "z", "i", "j", "k", "l",
-                           "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "ts",
-                           "ch", "sh", "shch", "", "y", "'", "e", "yu", "ya"};
+                           "a", "b", "v", "g", "d", "e", "zh", "z", "i", "j", "k",
+                           "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h",
+                           "ts","ch", "sh", "shch", "", "y", "'", "e", "yu", "ya"};
 
     int length = 0;
     for (int i = 0; s[i] != '\0'; i++){
@@ -25,15 +25,14 @@ char *translit(const unsigned char *s) {
     char *output = new char [length * 5];
     int i = 0;
     int j = 0;
-    int pos;
-    char *letter = new char [3];
+    short pos;
+    unsigned char *letter = new unsigned char [3];
     while (i < length) {
         if ((s[i] == 0xd0 && s[i + 1] >= 0x90 && s[i + 1] <= 0xbf) || (s[i] == 0xd1 && s[i + 1] >= 0x80 && s[i + 1] <= 0x8f)){
             letter[0] = s[i];
             letter[1] = s[i + 1];
             letter[2] = '\0';
             for (pos = 0; strcmp((const char*)rus[pos], (const char*)letter) != 0; pos++);
-            if (pos >= 41){pos++;}
             memcpy(output + j, eng[pos], strlen(eng[pos]));
             j += strlen(eng[pos]);
             i += 2;
