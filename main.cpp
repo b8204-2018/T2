d94fd6092afbd8967954e92d0e7fae8c9d2d9e66
metovashka <karina_matevosyan1999@mail.ru>
2019-01-13 16:31:05 +1000

    I competed Task2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <cstring>
+#include <cassert>
 #include "src/translit.h"
 
+using namespace std;
+
 int main() {
-    assert(strcmp("privet", translit((const unsigned char *) "привет")) == 0);
+
+    assert(strcmp("VPERED, VPERED, NA ZOV SOHATIH", translit((const unsigned char *) "ВПЕРЕД, ВПЕРЕД, НА ЗОВ СОХАТЫХ")) == 0);
+    assert(strcmp("NAVSTRECHU BESKONECHNIM MIRAM!", translit((const unsigned char *) "НАВСТРЕЧУ БЕСКОНЕЧНЫМ МИРАМ!")) == 0);
+    assert(strcmp("VPERED, VO IMYA VSEH PERNATIH", translit((const unsigned char *) "ВПЕРЕД, ВО ИМЯ ВСЕХ ПЕРНАТЫХ")) == 0);
+    assert(strcmp("NA POMOSCH' CHARODEYAM-KOTAM!", translit((const unsigned char *) "НА ПОМОЩЬ ЧАРОДЕЯМ-КОТАМ!")) == 0);
+
     return 0;
 }
+
+
--- src/translit.cpp
+++ src/translit.cpp
@@ -1,6 +1,29 @@
+#include <iostream>
 #include "translit.h"
 
+using namespace std;
+
 char *translit(const unsigned char *s) {
-    /** TODO: paste your code here */
-    return nullptr;
+    const char Alph[][4] = { "A", "B", "V", "G", "D", "E", "ZH", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F",
+                             "H", "TS", "CH", "SH", "SCH", "", "I", "'", "E", "YU", "YA"};
+
+    int n = 0;
+    for (int i = 0; s[i] != '\0'; i++, n++);
+    char *result = new char[n * 4];
+    int  j = 0;
+    for (int i(0); i < n; i++) {
+        if (s[i] == 0xd0 || s[i] == 0xd1) {
+            int pos = (s[i] << 8) + s[i + 1];
+            pos -= 0xd090;// номер символа для транслита в массиве
+            memcpy(result + j, Alph[pos], strlen(Alph[pos]));
+            j += strlen(Alph[pos]);
+            i++;
+        }
+        else {
+            memcpy(result + j, s + i, 1);// если не является буквой русского алфавита
+            j++;
+        }
+    }
+    result[j] = '\0';
+    return result;
 }
