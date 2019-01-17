bc82931ebffd92d7cbfc6a3e9a3b00ffaf5d313e
katebekk <44308249+katebekk@users.noreply.github.com>
2019-01-17 18:23:36 +1000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <cstring>
+#include <cassert>
 #include "src/translit.h"
 
 int main() {
     assert(strcmp("privet", translit((const unsigned char *) "привет")) == 0);
+	assert(strcmp("prIIIvet", translit((const unsigned char *) "прИИИвет")) == 0);
+	assert(strcmp("privet CHELOVEK", translit((const unsigned char *) "привет ЧЕЛОВЕК")) == 0);
+	assert(strcmp("privettt", translit((const unsigned char *) "приветТТТ")) == 0);
+	
     return 0;
 }
