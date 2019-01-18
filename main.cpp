d962934754a163beb116f23ab20dddb3d76542c2
Kirill-Mokhov <mokhovichenkokirill@gmail.com>
2019-01-18 16:25:54 +1000

    T2 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <cstring>
+#include <cassert>
 #include "src/translit.h"
 
 int main() {
     assert(strcmp("privet", translit((const unsigned char *) "привет")) == 0);
+    assert(strcmp("KIRILL", translit((const unsigned char *) "КИРИЛЛ")) == 0);
+    assert(strcmp("MOHOVIChENKO", translit((const unsigned char *) "МОХОВИЧЕНКО")) == 0);
+    assert(strcmp("Dom", translit((const unsigned char *) "Дом")) == 0);
+    assert(strcmp("Vosem'", translit((const unsigned char *) "Восемь")) == 0);
+    assert(strcmp("Poezd", translit((const unsigned char *) "Поезд")) == 0);
+    assert(strcmp("Ssylka", translit((const unsigned char *) "Ссылка")) == 0);
+    assert(strcmp("Aloe", translit((const unsigned char *) "Алоэ")) == 0);
     return 0;
 }
