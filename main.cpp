ba0efd512b5af3e652d55831a35a62c18f54f2cb
Замятин Дмитрий <Dmitry.jabkevich@gmail.com>
2019-01-19 15:07:38 +1000

    ...

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <cstring>
 #include "src/translit.h"
-
+#include <cassert>    
+using namespace std;
+//																				B=0xd0 0x91 П = 0xd0 0x9f
 int main() {
-    assert(strcmp("privet", translit((const unsigned char *) "привет")) == 0);
+	assert(strcmp("PRIVET", translit((const unsigned char *) "ПРИВЕТ")) == 0);
+	system("pause");
     return 0;
+
 }
--- src/translit.cpp
+++ src/translit.cpp
@@ -1,6 +1,35 @@
 #include "translit.h"
+#include<string>
+#include <cstring>
+
+using namespace  std;
 
 char *translit(const unsigned char *s) {
-    /** TODO: paste your code here */
-    return nullptr;
+	const char Alph[][4] = { "A", "B", "V", "G", "D", "E", "ZH", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F",
+								 "H", "TS", "CH", "SH", "SCH", "", "I", "'", "E", "YU", "YA" };
+
+	int n = 0;
+	int pos = 0;
+	while (s[n] != '\0') {
+		n++;
+	}
+	
+	char *result = new char[n * 4];
+	int  j = 0;
+	for (int i=0; i < n; i++) {
+		if (s[i] == 0xd0 || s[i] == 0xd1) { //
+			pos = s[i + 1] - 0x90;// номер символа для транслита в массиве
+			memcpy(result + j, Alph[pos], strlen(Alph[pos]));
+			j += strlen(Alph[pos]);
+			i++;
+		}
+		else {
+			memcpy(result + j, s + i, 1);// если не является буквой русского алфавита
+			j++;
+		}
+	}
+	result[j] = '\0';
+	return result;
+
 }
+
