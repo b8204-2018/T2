d1b2f788b02b24cdf7c89b05a01ac2ea04fed7d2
IvanUstiugov <ustyugovdvfu@gmail.com>
2019-01-25 11:10:57 +1000

    Work translit for main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cstring>
 #include <cassert>
+
 #include "src/translit.h"
+#include "src/translit.cpp"
 
 int main () {
+assert (strcmp ("Moloko vkusnoe", translit((const unsigned char *) "Молоко вкусное")) == 0);
 
-  assert (strcmp ("Moloko vkusnoe", translit((const unsigned char *) "Молоко вкусное")) == 0);
-
+std::cout << "\n\nВыполняем транслит\n\n";
+std::cout << translit((const unsigned char *) "Молоко вкусное") << std::endl;
 
   return 0;
 
